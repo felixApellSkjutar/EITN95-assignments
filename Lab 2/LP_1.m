655ab3794b7b724383d4b404fb835e96b0e1bead
Aleko Lilius <aleko.lilius@gmail.com>
2023-04-25 15:34:40 +0200

    Task1 for lab2 in motion

--- Lab 2/LP_1.m
+++ Lab 2/LP_1.m
@@ -1,16 +1,41 @@
 clc
 clear all
 
-c = [-100;
-     -150];
-A = [8000 4000;
-     15   30 ];
- b = [40000;
-      200];
- lb = [0;
-       0];
- %options = optimoptions('linprog', 'Algorithm', 'interior-point', 'Display', 'iter');
- %options = optimoptions('linprog', 'Algorithm', 'interior-point', 'Display', 'off');
- %options = optimoptions('linprog', 'Algorithm', 'dual-simplex', 'Display', 'iter');
- options = optimoptions('linprog', 'Algorithm', 'dual-simplex', 'Display', 'off');
- [x,fval,exitflag,output,lambda] = linprog(c', A, b, [], [], lb, [], [], options)
+c = [-13;
+    -11];
+A = [4 5;
+    5 3;
+    1 2];
+b = [1500;
+    1575;
+    420];
+lb = [0;
+    0];
+%options = optimoptions('linprog', 'Algorithm', 'interior-point', 'Display', 'iter');
+options = optimoptions('linprog', 'Algorithm', 'interior-point', 'Display', 'off');
+[x, fval] = linprog(c', A, b, [], [], lb, [], [], options);
+
+disp('Optimal maximum solution x for task 1:');
+disp(x);
+disp('Optimal maximum function value for task 1:');
+disp(-fval);
+
+
+c = [-1500;
+    -1575;
+    -420];
+A = [4 5 1;
+    5 3 2];
+b = [13;
+    11];
+lb = [0;
+    0;
+    0];
+%options = optimoptions('linprog', 'Algorithm', 'dual-simplex', 'Display', 'iter');
+options = optimoptions('linprog', 'Algorithm', 'dual-simplex', 'Display', 'off');
+[y, fval] = linprog(c', A, b, [], [], lb, [], [], options);
+
+disp('Optimal minimum solution x for task 2:');
+disp(y);
+disp('Optimal minimum function value for task 2:');
+disp(fval);
