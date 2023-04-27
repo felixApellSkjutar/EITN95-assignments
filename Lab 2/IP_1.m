df4a639d236a55e32e5db932d1b49a3371f1c910
Tove Börjeson <tove.borjeson@gmail.com>
2023-04-27 15:02:56 +0200

    Lab 2: Task 2 Matlab

--- Lab 2/IP_1.m
+++ Lab 2/IP_1.m
@@ -1,16 +1,83 @@
+%% 2.1 
 clc
 clear all
 
-c = [-100;
-     -150];
-A = [8000 4000;
-     15   30 ];
- b = [40000;
-      200];
+c = [1;
+     1;
+     1;
+     1;
+     1;
+     1;
+     1];
+ 
+A = [1 0 0 1 1 1 1;
+    1 1 0 0 1 1 1;
+    1 1 1 0 0 1 1;
+    1 1 1 1 0 0 1;
+    1 1 1 1 1 0 0;
+    0 1 1 1 1 1 0;
+    0 0 1 1 1 1 1
+    ];
+ b = [8;
+      6;
+      5;
+      4;
+      6;
+      7;
+      9];
  lb = [0;
+       0;
+       0;
+       0;
+       0;
+       0;
        0];
  %options = optimoptions('intlinprog', 'Display', 'iter');
  options = optimoptions('intlinprog', 'Display', 'off');
  intcon = [1;
-           2];
-[x, fval, exitflag, output] = intlinprog(c', intcon, A, b, [], [], lb, [], options)
+           2;
+           3;
+           4;
+           5;
+           6;
+           7];
+[x, fval, exitflag, output] = intlinprog(c', intcon, -A, -b, [], [], lb, [], options)
+
+%% 2.2
+clc
+clear all
+
+c = [1;
+     1;
+     1;
+     1;
+     1;
+     1;
+     1];
+ 
+A = [1 0 0 1 1 1 1;
+    1 1 0 0 1 1 1;
+    1 1 1 0 0 1 1;
+    1 1 1 1 0 0 1;
+    1 1 1 1 1 0 0;
+    0 1 1 1 1 1 0;
+    0 0 1 1 1 1 1
+    ];
+ b = [8;
+      6;
+      5;
+      4;
+      6;
+      7;
+      9];
+ lb = [0;
+       0;
+       0;
+       0;
+       0;
+       0;
+       0];
+ %options = optimoptions('intlinprog', 'Display', 'iter');
+ options = optimoptions('intlinprog', 'Display', 'off');
+
+[x, fval, exitflag, output] = linprog(c', -A, -b, [], [], lb, [], options)
