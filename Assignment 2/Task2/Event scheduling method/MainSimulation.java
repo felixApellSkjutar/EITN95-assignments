fe8bace60fe17de872946942f6b15e98c5c5d9be
FelixApellSkjutar <felix.apellskjutar@gmail.com>
2023-05-05 16:01:30 +0200

    task2 done

--- Assignment 2/Task2/Event scheduling method/MainSimulation.java
+++ Assignment 2/Task2/Event scheduling method/MainSimulation.java
@@ -1,6 +1,4 @@
 import java.util.*;
-import java.util.stream.DoubleStream;
-import java.util.stream.Stream;
 import java.io.*;
 
 
@@ -13,9 +11,10 @@ public class MainSimulation extends GlobalSimulation{
 		List<Double> serviceTimes = new ArrayList<Double>();
         // The main simulation loop
 		State actState = new State(); 
-    	for (int i = 0; i < 1000; i++){
-			double startingTime = 9.0*60; 
-			time = startingTime;
+    	for (int i = 0; i < 10000; i++){
+			double startingTime = 9.0*60; //First customer arrives at 9:00 
+			time = startingTime; 
+
 			insertEvent(ARRIVAL, startingTime); 
 			
 			while (true){
@@ -23,44 +22,14 @@ public class MainSimulation extends GlobalSimulation{
 				time = actEvent.eventTime;
 				actState.treatEvent(actEvent);
 				if (time >= actState.maxArrivalTime && actState.numberInQueue == 0){
-					//System.out.println("in ready: " + actState.r + " arr: " + actState.arr + " numIn Kö: " + actState.numberInQueue + " time: " + time + " arrtimes " + actState.arrivalTimes );
 					break;
 				}
 			}
 			serviceTimes.addAll(actState.qTimes);
-			//System.out.println("nmKö: " +actState.numberInQueue);
 			endTimes.add(time);
-			actState = new State();
-			time = 0;
+			actState = new State(); //Every day we start a new simulation
     	}
     	
-		
-		//List<Double> ds = actState.iterations;
-		//DoubleStream ds = actState.iterations.stream().mapToDouble(Double::doubleValue);
-    	// System.out.println(actState.iterations);
-		
-		// long N = ds.stream().count();
-		// double mean = ds.stream().mapToDouble(Double::doubleValue).sum() / N;
-		// double variance = ds.stream().mapToDouble(Double::doubleValue).map(x -> x - mean).map(x -> x * x).sum() / N;
-		// double stdDev = Math.sqrt(variance);
-		// double confInt = 1.96 * stdDev / Math.sqrt(N);
-
-		// List<Double> prescTimes = actState.prescTimes;
-		// long Nb = prescTimes.stream().count();
-		// double meanB = prescTimes.stream().mapToDouble(Double::doubleValue).sum() / Nb;
-		// double varianceB = prescTimes.stream().mapToDouble(Double::doubleValue).map(x -> x - meanB).map(x -> x * x).sum() / Nb;
-		// double stdDevB = Math.sqrt(varianceB);
-		// double confIntB = 1.96 * stdDevB / Math.sqrt(Nb);
-
-		
-		// List<Double> queuetimes = actState.qTimes;
-		// Double max = queuetimes.stream().mapToDouble(Double::doubleValue).max().getAsDouble();
-		// Double min = queuetimes.stream().mapToDouble(Double::doubleValue).min().getAsDouble();
-		// long NQ = queuetimes.stream().count();
-		// double meanQ = queuetimes.stream().mapToDouble(Double::doubleValue).sum() / NQ;
-		// double varianceQ = queuetimes.stream().mapToDouble(Double::doubleValue).map(x -> x - meanQ).map(x -> x * x).sum() / NQ;
-		// double stdDevQ = Math.sqrt(varianceQ);
-		// double confIntQ = 1.96 * stdDevQ / Math.sqrt(NQ);
 		long N = endTimes.stream().count();
 		double mean = endTimes.stream().mapToDouble(Double::doubleValue).sum() / N;
 		double variance = endTimes.stream().mapToDouble(Double::doubleValue).map(x -> x - mean).map(x -> x * x).sum() / N;
@@ -100,18 +69,6 @@ public class MainSimulation extends GlobalSimulation{
 		System.out.println("max: " + maxB);
 		System.out.println("min: " + minB);
 		
-		
-		// System.out.println("Data size: " + NQ);
-		// System.out.println("max: " + max);
-		// System.out.println("min: " + min);
-		// System.out.println("Mean: " + meanQ);
-		// System.out.println("Variance: " + varianceQ);
-		// System.out.println("Standard deviation: " + stdDevQ);
-		// System.out.println("Confidence interval: " + confIntQ);
-		// System.out.println("Confidence interval upper bound: " + (meanQ + confIntQ));
-		// System.out.println("Confidence interval lower bound: " + (meanQ - confIntQ));
-
-//		System.out.println(endTimes);
     }
 	
 }
--- Assignment 2/Task2/Event scheduling method/State.java
+++ Assignment 2/Task2/Event scheduling method/State.java
@@ -1,45 +1,32 @@
 import java.util.*;
-import java.io.*;
 
 class State extends GlobalSimulation{
 	
-	// Here follows the state variables and other variables that might be needed
-	// e.g. for measurements
-	public int  numberInQueue = 0, accumulated = 0, noMeasurements = 0;
+	public int  numberInQueue = 0;
 
 	Random slump = new Random(); // This is just a random number generator
 	
-	int arr = 0, r = 0;
-	double lambda = 4.0/60, lastArrival = 0, qSize = 0; //mean inter-arrival time: 4/hour
-	double maxArrivalTime = 17*60.0; //the pharmacy is open for 8h
-	public List<Double> iterations = new ArrayList<Double>();
+	double lambda = 4.0/60; //mean inter-arrival time: 4 customers per hour
+	double maxArrivalTime = 17*60.0; //the pharmacy is open until 17:00
+	
 	public List<Double> qTimes = new ArrayList<Double>();
 	public LinkedList<Double> arrivalTimes = new LinkedList<>();
-	// The following method is called by the main program each time a new event has been fetched
-	// from the event list in the main loop. 
+
 	public void treatEvent(Event x){
 		switch (x.eventType){
 			case ARRIVAL:
 				if (time <= maxArrivalTime) {
-					arr++;
 					arrival();
 				}
 				break;
 			case READY:
-				r++;
 				ready();
 				break;
 		}
 	}
 	
-	
-	// The following methods defines what should be done when an event takes place. This could
-	// have been placed in the case in treatEvent, but often it is simpler to write a method if 
-	// things are getting more complicated than this.
-	
 	private void arrival(){
 		arrivalTimes.addLast(time);
-		qSize = numberInQueue;
 		if (numberInQueue == 0){
 			double serviceTime = prescriptonFill(slump);
 			insertEvent(READY, time + serviceTime);
@@ -50,21 +37,18 @@ class State extends GlobalSimulation{
 	}
 	
 	private void ready(){
-		lastArrival = time;
 		qTimes.add(time - arrivalTimes.pop());
 		numberInQueue--;
 		if (numberInQueue > 0){
 			double serviceTime = prescriptonFill(slump);
 			insertEvent(READY, time + serviceTime);
 		}
-		//System.out.println("in ready: " + r + " arr: " + arr + " numIn Kö: " + numberInQueue + " time: " + time);
 	}
 
 
 	private double expDist(double lambda) {
 		double u = slump.nextDouble();
-		double mm = Math.log(1 - u)*(-1.0/lambda);
-		return mm;
+		return Math.log(1 - u)*(-1.0/lambda);
 	}
 
 
